c2b32ae862982927974ce7580f92d9fcacbc056b
Mürvet <98389201+mrvetarslan@users.noreply.github.com>
2022-08-26 02:09:24 +0300

    cikti sorusu

---
+++ Enum-Struct/lab8s16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct player{
+    char pname[20];
+}p1;
+
+char* play(struct player *temp_p1){
+    strcpy(temp_1->pname,"kohli");
+    return temp_p1->pname;
+}
+int main(int argc, char *argv[]) {
+    strcpy(p1.pname,"dhoni");
+    printf("%s %s",p1.pname,play(&p1));
+	return 0;
+}
